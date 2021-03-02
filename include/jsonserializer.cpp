8b2663cd0eefaadd5ec71e154f66ec20748078c5
fallen ink <fengzilijie@qq.com>
2021-03-02 10:18:21 +0800

    +

---
+++ include/jsonserializer.cpp
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-20 19:01:39
+ * @LastEditTime: 2021-02-20 19:02:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /slothjson/include/jsonserializer.cpp
+ */
+
+// 抽象json序列化的基类
+// 实现一个基于rapidjson的序列化器
---
+++ include/jsonserializer.h
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-20 19:01:30
+ * @LastEditTime: 2021-02-20 19:01:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /slothjson/include/jsonserializer.h
+ */
